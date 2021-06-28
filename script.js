8513a5b13f6a9bcbd90f41b3ed12db9bcf5a0b8a
utkarsh265 <utkarshtyagi6121@gmail.com>
2021-06-28 20:14:21 +0530

    3 files modified

--- script.js
+++ script.js
@@ -8,7 +8,7 @@ function display() {
   var width = document.getElementById('width').value;
 
   console.log(x_cord, y_cord, text, height, width);
-  var span = document.getElementById('span');
+  let span = document.getElementById('span');
   span.innerHTML = text;
   span.style.position = 'absolute';
 
@@ -20,7 +20,7 @@ function display() {
 
       span.style.top = y_cord + 'px';
       span.style.left = x_cord + 'px';
-      span.style.display = 'block';
+      // span.style.display = 'block'
     } else {
       alert('assign y-coordinate in range');
       span.style.display = 'none';
@@ -55,4 +55,43 @@ function output() {
 
 function refreshPage() {
   window.location.reload();
+}
+
+// setting color dynamically
+
+function change_color() {
+  document.getElementById('span').style.color = document.getElementById(
+    'color'
+  ).value;
+}
+
+// Drag & drop element
+
+const el = document.querySelector('.span');
+el.addEventListener('mousedown', mousedown);
+
+function mousedown(e) {
+  window.addEventListener('mousemove', mousemove);
+  window.addEventListener('mouseup', mouseup);
+
+  let prevX = e.clientX;
+  let prevY = e.clientY;
+
+  function mousemove(e) {
+    let newX = prevX - e.clientX;
+    let newY = prevY - e.clientY;
+
+    const rect = el.getBoundingClientRect();
+
+    el.style.left = rect.left - newX + 'px';
+    el.style.top = rect.top - newY + 'px';
+
+    prevX = e.clientX;
+    prevY = e.clientY;
+  }
+
+  function mouseup() {
+    window.removeEventListener('mousemove', mousemove);
+    window.removeEventListener('mouseup', mouseup);
+  }
 }
